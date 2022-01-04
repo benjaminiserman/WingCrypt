34551226d93e42c882bdaa08b0e204714881ff33
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-04 15:09:39 -0600

    switch extension to .wenc

--- SharedConstants.cs
+++ SharedConstants.cs
@@ -4,7 +4,7 @@ using System.Text;
 internal static class SharedConstants
 {
 	internal const string WORKING_NAME = "_wingcrypttemp.zip";
-	internal const string FILETYPE = ".enc";
+	internal const string FILETYPE = ".wenc";
 
 	internal static byte[] IV => new byte[] { 69, 42, 39, 0, 3, 13, 87, 27, 121, 62, 1, 111, 245, 73, 199, 154 };
 	// initialization vector bytes courtesy of Kraber Queen
