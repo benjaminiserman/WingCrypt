a8ac47f202bd999c4c0b14f118b31a233b547c42
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-17 15:55:54 -0600

    initial

---
+++ WingCryptXamarin/WingCryptXamarin/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace WingCryptXamarin;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+	protected override void OnStart()
+	{
+	}
+
+	protected override void OnSleep()
+	{
+	}
+
+	protected override void OnResume()
+	{
+	}
+}
---
+++ WingCryptXamarin/WingCryptXamarin/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace WingCryptXamarin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
