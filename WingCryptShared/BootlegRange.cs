154fefe54223f283bd45ca1bea79df7b37ef3248
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-17 16:08:03 -0600

    downgrade WingCryptShared to netstandard2.0

---
+++ WingCryptShared/BootlegRange.cs
@@ -0,0 +1,19 @@
+﻿namespace WingCryptShared;
+public static class BootlegRange
+{
+	// [x..y]
+	public static T[] Sub<T>(this T[] array, int start, int end)
+	{
+		T[] output = new T[end - start];
+
+		for (int i = start; i < end; i++)
+		{
+			output[i - start] = array[i];
+		}
+
+		return output;
+	}
+
+	// [x..^y]
+	public static T[] SubFromEnd<T>(this T[] array, int start, int end) => Sub(array, start, array.Length - end);
+}
