dfd65434873527357b499e918663f12efd4ca74b
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-25 20:43:07 -0600

    add android splash screen

--- WingCryptXamarin/WingCryptXamarin.Android/MainActivity.cs
+++ WingCryptXamarin/WingCryptXamarin.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace WingCryptXamarin.Droid
 {
-    [Activity(Label = "WingCryptXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "WingCryptXamarin", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
