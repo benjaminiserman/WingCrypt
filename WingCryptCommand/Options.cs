65975f1df8da037cd5e9cb2ab62fda373ac65870
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-02 18:23:49 -0600

    add implicit verb

--- WingCryptCommand/Options.cs
+++ WingCryptCommand/Options.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
 
-[Verb("encrypt", HelpText = "Encrypts the provided path")]
+[Verb("encrypt", HelpText = "Encrypts the provided path.")]
 internal class EncryptOptions
 {
 	[Option('p', "path", Required = true, HelpText = "The path to be encrypted.")]
@@ -19,7 +19,7 @@ internal class EncryptOptions
 	public bool Delete { get; set; }
 }
 
-[Verb("decrypt", HelpText = "Decrypt the provided path")]
+[Verb("decrypt", HelpText = "Decrypt the provided path.")]
 internal class DecryptOptions
 {
 	[Option('p', "path", Required = true, HelpText = "The path to be decrypted.")]
@@ -30,4 +30,45 @@ internal class DecryptOptions
 
 	[Option('d', "delete", Default = false, HelpText = "Delete path after decryption is complete.")]
 	public bool Delete { get; set; }
+}
+
+[Verb("do", isDefault: true, Hidden = false, HelpText = "Decrypts a .wenc file, or encrypts anything else.")]
+internal class DoOptions
+{
+	[Option('p', "path", Required = true, HelpText = "The path to be encrypted/decrypted.")]
+	public string Path { get; set; }
+
+	[Option('k', "key", Required = true, HelpText = "The password (not technically a key) to be used in encryption/decryption.")]
+	public string Password { get; set; }
+
+	[Option('d', "delete", Default = false, HelpText = "Delete path after encryption/decryption is complete.")]
+	public bool Delete { get; set; }
+}
+
+internal class Options
+{
+	public string Path { get; private set; }
+	public string Password { get; private set; }
+	public bool Delete { get; private set; }
+
+	public Options(EncryptOptions e)
+	{
+		Path = e.Path;
+		Password = e.Password;
+		Delete = e.Delete;
+	}
+
+	public Options(DecryptOptions e)
+	{
+		Path = e.Path;
+		Password = e.Password;
+		Delete = e.Delete;
+	}
+
+	public Options(DoOptions e)
+	{
+		Path = e.Path;
+		Password = e.Password;
+		Delete = e.Delete;
+	}
 }
--- WingCryptCommand/Program.cs
+++ WingCryptCommand/Program.cs
@@ -7,40 +7,55 @@ public static class Program
 {
 	public static void Main(string[] args)
 	{
-		var result = Parser.Default.ParseArguments<EncryptOptions, DecryptOptions>(args)
-			.WithParsed<EncryptOptions>(options =>
+		var result = Parser.Default.ParseArguments<EncryptOptions, DecryptOptions, DoOptions>(args)
+			.WithParsed<EncryptOptions>(options => Encrypt(new(options)))
+			.WithParsed<DecryptOptions>(options => Decrypt(new(options)))
+			.WithParsed<DoOptions>(options =>
 			{
-				try
+				if (options.Path.Length > SharedConstants.FILETYPE.Length && options.Path[^SharedConstants.FILETYPE.Length..] == SharedConstants.FILETYPE)
 				{
-					SingleEntry entry = new(options.Path);
-					Encryptor.Encrypt(entry, Path.Combine(options.Path.Split('\\')[..^1]), options.Password);
-
-					if (options.Delete)
-					{
-						string path = entry.EnumerateFiles().First();
-
-						if (Directory.Exists(path)) Directory.Delete(path, true);
-						else if (File.Exists(path)) File.Delete(path);
-					}
+					Decrypt(new(options));
 				}
-				catch (IOException)
+				else
 				{
-					Console.WriteLine($"Path {options.Path} was not found.");
-				}
-			})
-			.WithParsed<DecryptOptions>(options =>
-			{
-				try
-				{
-					SingleEntry entry = new(options.Path);
-					Decryptor.Decrypt(options.Path, entry.Name, options.Password);
-
-					if (options.Delete) File.Delete(entry.EnumerateFiles().First());
-				}
-				catch (IOException)
-				{
-					Console.WriteLine($"Path {options.Path} was not found.");
+					Encrypt(new(options));
 				}
 			});
+	}
+
+	private static void Encrypt(Options options)
+	{
+		try
+		{
+			SingleEntry entry = new(options.Path);
+			Encryptor.Encrypt(entry, Path.Combine(options.Path.Split('\\')[..^1]), options.Password);
+
+			if (options.Delete)
+			{
+				string path = entry.EnumerateFiles().First();
+
+				if (Directory.Exists(path)) Directory.Delete(path, true);
+				else if (File.Exists(path)) File.Delete(path);
+			}
+		}
+		catch (IOException)
+		{
+			Console.WriteLine($"Path {options.Path} was not found.");
+		}
+	}
+
+	private static void Decrypt(Options options)
+	{
+		try
+		{
+			SingleEntry entry = new(options.Path);
+			Decryptor.Decrypt(options.Path, entry.Name, options.Password);
+
+			if (options.Delete) File.Delete(entry.EnumerateFiles().First());
+		}
+		catch (IOException)
+		{
+			Console.WriteLine($"Path {options.Path} was not found.");
+		}
 	}
 }
